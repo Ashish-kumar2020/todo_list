39a84723566458b3fbf0b310df3d6026745a4598
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2025-05-03 00:23:16 +0530

    designed signin component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Signup from "./components/Signup/Signup";
-
+import Signin from "./components/Signin/Signin";
 function App() {
   return (
     <>
       <Header />
-      <Signup />
+      {/* <Signup /> */}
+      <Signin />
     </>
   );
 }
---
+++ frontend/src/components/Signin/Signin.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import {
+  Text,
+  Button,
+  Box,
+  Stack,
+  HStack,
+  Icon,
+  Field,
+  Input,
+  InputGroup,
+  Image,
+  Grid,
+} from "@chakra-ui/react";
+import { FcGoogle } from "react-icons/fc";
+import {
+  FaFacebook,
+  FaApple,
+  FaLock,
+  FaVoicemail,
+  FaCalendarAlt,
+} from "react-icons/fa";
+import "./Signin.css";
+
+const Signin = () => {
+  return (
+    <div className="signin-container">
+      <div className="signin-heading">
+        <Text fontSize="32px" fontWeight="bold" mb={4}>
+          Signin
+        </Text>
+
+        <div className="form-container">
+          <Box mt="10px">
+            <InputGroup startElement={<FaVoicemail />}>
+              <Input placeholder="Email" minWidth="60px" maxWidth="400px" />
+            </InputGroup>
+          </Box>
+          <Box mt="10px">
+            <InputGroup startElement={<FaLock />}>
+              <Input placeholder="Password" minWidth="60px" maxWidth="400px" />
+            </InputGroup>
+          </Box>
+
+          <Box mt="20px">
+            <Button
+              w="100%"
+              fontWeight="bold"
+              fontSize="md"
+              variant="outline"
+              borderRadius="lg"
+              py={6}
+              bg="#DB4C3F"
+            >
+              Login
+            </Button>
+          </Box>
+          <Box mt="20px">
+            <Text fontSize="13px">
+              By continuing with Google, Apple, or Email, you agree to Todoist’s{" "}
+              <br />
+              <Text
+                as="span"
+                textDecoration="underline"
+                color="#DB4C3F"
+                fontWeight="medium"
+                cursor="pointer"
+              >
+                Terms of Service
+              </Text>{" "}
+              and
+              <Text
+                as="span"
+                textDecoration="underline"
+                color="#DB4C3F"
+                fontWeight="medium"
+                ml="4px"
+                cursor="pointer"
+              >
+                Privacy Policy.
+              </Text>{" "}
+            </Text>
+          </Box>
+          <Box mt="20px" textAlign="center">
+            <Text fontSize="13px">
+              Don’t have an account?{" "}
+              <Text
+                as="span"
+                textDecoration="underline"
+                color="#DB4C3F"
+                fontWeight="medium"
+                cursor="pointer"
+              >
+                Signup
+              </Text>
+            </Text>
+          </Box>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signin;
