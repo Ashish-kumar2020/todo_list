b8767d2cd0ea04e62e6482fb9364431cad012e1c
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2025-05-08 23:13:35 +0530

    created todo dispay layout

--- frontend/src/components/HomePage/HomePage.jsx
+++ frontend/src/components/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Text,
   Container,
@@ -6,37 +6,62 @@ import {
   InputGroup,
   Accordion,
   Span,
+  Box,
+  Button,
 } from "@chakra-ui/react";
-import { LuCalendarPlus } from "react-icons/lu";
+import "./HomePage.css";
+import CreateTodo from "../Todo/CreateTodo";
 const HomePage = () => {
   const items = [
     { value: "a", title: "First Item", text: "Some value 1..." },
     { value: "b", title: "Second Item", text: "Some value 2..." },
     { value: "c", title: "Third Item", text: "Some value 3..." },
   ];
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const handleShowTodoComponent = () => {
+    setIsDrawerOpen(true);
+  };
+
+  const handleCloseDrawer = () => {
+    setIsDrawerOpen(false);
+  };
   return (
-    <div>
-      <Text fontSize="32px" fontWeight="bold" mb={4}>
-        Todo
-      </Text>
+    <div className="todo-container">
       <div>
+        <Text fontSize="32px" fontWeight="bold" mb={4}>
+          Add Todo
+        </Text>
         <Container>
-          <InputGroup startElement={<LuCalendarPlus />} w={800}>
-            <Input placeholder="Add New Task" />
-          </InputGroup>
-          <Accordion.Root collapsible w={800}>
-            {items.map((item, index) => (
-              <Accordion.Item key={index} value={item.value}>
-                <Accordion.ItemTrigger>
-                  <Span flex="1">{item.title}</Span>
-                  <Accordion.ItemIndicator />
-                </Accordion.ItemTrigger>
-                <Accordion.ItemContent>
-                  <Accordion.ItemBody>{item.text}</Accordion.ItemBody>
-                </Accordion.ItemContent>
-              </Accordion.Item>
-            ))}
-          </Accordion.Root>
+          <Box mt="30px">
+            <Button
+              w="100%"
+              fontWeight="bold"
+              fontSize="md"
+              variant="outline"
+              borderRadius="lg"
+              py={6}
+              bg="#DB4C3F"
+              onClick={handleShowTodoComponent}
+            >
+              Add Task
+            </Button>
+          </Box>
+          <CreateTodo isOpen={isDrawerOpen} onClose={handleCloseDrawer} />
+          <div className="todo-display">
+            <Accordion.Root collapsible w={800}>
+              {items.map((item, index) => (
+                <Accordion.Item key={index} value={item.value}>
+                  <Accordion.ItemTrigger>
+                    <Span flex="1">{item.title}</Span>
+                    <Accordion.ItemIndicator />
+                  </Accordion.ItemTrigger>
+                  <Accordion.ItemContent>
+                    <Accordion.ItemBody>{item.text}</Accordion.ItemBody>
+                  </Accordion.ItemContent>
+                </Accordion.Item>
+              ))}
+            </Accordion.Root>
+          </div>
         </Container>
       </div>
     </div>
--- frontend/src/components/Todo/CreateTodo.jsx
+++ frontend/src/components/Todo/CreateTodo.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Button, CloseButton, Drawer, Portal } from "@chakra-ui/react";
 
-const CreateTodo = () => {
-  return <div>CreateTodo</div>;
+const CreateTodo = ({ isOpen, onClose }) => {
+  return (
+    <Drawer.Root open={isOpen} onClose={onClose} size={"md"}>
+      <Portal>
+        <Drawer.Backdrop />
+        <Drawer.Positioner>
+          <Drawer.Content>
+            <Drawer.Header>
+              <Drawer.Title>Drawer Title</Drawer.Title>
+              <Drawer.CloseTrigger asChild>
+                <CloseButton size="sm" onClick={onClose} />
+              </Drawer.CloseTrigger>
+            </Drawer.Header>
+            <Drawer.Body>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </Drawer.Body>
+            <Drawer.Footer>
+              <Button variant="outline" onClick={onClose}>
+                Cancel
+              </Button>
+              <Button>Save</Button>
+            </Drawer.Footer>
+          </Drawer.Content>
+        </Drawer.Positioner>
+      </Portal>
+    </Drawer.Root>
+  );
 };
 
 export default CreateTodo;
